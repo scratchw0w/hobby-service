dde58d852139c4af509f403305b8c35990942bf8
Maksym <rikle_ng@ukr.net>
2022-11-10 13:46:02 +0200

    Added like and dislike hobbies endpoints for LikedHobbyController.

--- src/main/java/com/scratchy/controller/LikedHobbyController.java
+++ src/main/java/com/scratchy/controller/LikedHobbyController.java
@@ -6,13 +6,15 @@ import com.scratchy.entity.Hobby;
 import com.scratchy.service.HobbyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/")
+@RequestMapping("/v1/wishlist")
 public class LikedHobbyController {
 
   private final HobbyService hobbyService;
@@ -20,5 +22,15 @@ public class LikedHobbyController {
   @GetMapping
   public ResponseEntity<List<Hobby>> getUserHobbyList(String userId) {
     return ResponseEntity.ok(List.of());
+  }
+
+  @PostMapping
+  public ResponseEntity<Hobby> likeHobby(Hobby hobby, String userId) {
+    return ResponseEntity.ok(hobby);
+  }
+
+  @DeleteMapping
+  public ResponseEntity<Hobby> dislikeHobby(Hobby hobby, String userId) {
+    return ResponseEntity.ok(hobby);
   }
 }
